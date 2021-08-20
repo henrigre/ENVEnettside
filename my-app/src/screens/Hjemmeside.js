64058bc3c3561b7d34151cb6c2648b3031f05698
henrigre <henrik_grevstad@outlook.com>
2021-08-20 13:04:37 +0200

    bilder

--- my-app/src/screens/Hjemmeside.js
+++ my-app/src/screens/Hjemmeside.js
@@ -6,7 +6,12 @@ function Hjemmeside() {
   return (
     <div>
       <nav className="navBar">{NavBar()}</nav>
-      <img className="shadowPhoto" src="/testHoved.png" width="100%" alt="" />
+      <img
+        className="shadowPhoto"
+        src="/animertStartbilde.jpg"
+        width="100%"
+        alt=""
+      />
       <div className="boxInfo">
         <button className="boxInfoText">
           <label className="text">
@@ -15,14 +20,14 @@ function Hjemmeside() {
             sammen med tunge lokale mappestrukturer. Domenekunnskap står sterkt
             og knytter opp kunnskap og risiko til den enkelte tilrettelegger.
             Summen av dagens virksomhet er en ekstremt ressurskrevende
-            papirmølle, uten noen gode løsninger.<span> </span> ENVE er en
-            programvare som inneholder de kritiske funksjonene som
-            tilrettelegger har behov for i sin arbeidshverdag. Med nødvendige
-            integrasjoner mot eksisterende systemer innen FDV klarer ENVE tilby
-            en one-stop-shop for forvaltningsprosessen av næringseiendom. På
-            denne måten vil vi fristille ressurser til mer verdiskapende arbeid
-            og bidra til digitaliseringen i en av verdens mest største
-            industrier.
+            papirmølle, uten noen gode løsninger.
+            <span className="paragraf"> Hei </span> ENVE er en programvare som
+            inneholder de kritiske funksjonene som tilrettelegger har behov for
+            i sin arbeidshverdag. Med nødvendige integrasjoner mot eksisterende
+            systemer innen FDV klarer ENVE tilby en one-stop-shop for
+            forvaltningsprosessen av næringseiendom. På denne måten vil vi
+            fristille ressurser til mer verdiskapende arbeid og bidra til
+            digitaliseringen i en av verdens mest største industrier.
           </label>
         </button>
       </div>
--- my-app/src/screens/NavBar.js
+++ my-app/src/screens/NavBar.js
@@ -1,5 +1,6 @@
 import { Link, NavLink } from "react-router-dom";
 import "../styles/NavBar.css";
+import { BiMoon } from "react-icons/bi";
 
 function NavBar() {
   return (
